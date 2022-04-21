797e4f37dcba5ac889e2d6b7f44087ed64475d3e
HetviSolanki49701 <hetvis.solanki@gmail.com>
2022-04-21 15:22:40 +0530

    more routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Alan from "./Alan";
+import Test from "./Test";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/alan" element={<Alan />} />       
+        <Route path="/test" element={<Test />} />       
       </Routes>
     </BrowserRouter>
   );
