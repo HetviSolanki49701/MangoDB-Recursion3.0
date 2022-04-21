6fb7698c7ae0e7d3cffedb56a03480481936eb75
HetviSolanki49701 <hetvis.solanki@gmail.com>
2022-04-22 04:34:54 +0530

    coaches page

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import Home from "./components/Home/Home";
 import { BrowserRouter, Route, NavLink, Link, Routes,useNavigate } from "react-router-dom";
 import Alan from "./Alan";
 import Profile from "./components/Profile/Profile";
-import { BrowserRouter, Route, NavLink, Link, Routes } from "react-router-dom";
 import Exercise from "./components/Exercise/Exercise";
 import Room from "./components/Room/Room";
 import React, { useEffect } from 'react';
+import Coach from "./components/Coach/Coach";
 
 function App() {
   return (
@@ -17,6 +17,7 @@ function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/exercise" element={<Exercise />} />
         <Route path="/room" element={<Room />} />
+        <Route path="/coach" element={<Coach />} />
       </Routes>
     </BrowserRouter>
   );
