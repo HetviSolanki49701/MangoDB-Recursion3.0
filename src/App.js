6c2039ccc3ef50b19c891f714f20d0aa2113e008
HetviSolanki49701 <hetvis.solanki@gmail.com>
2022-04-21 15:20:57 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
-
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Alan from "./Alan";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/alan" element={<Alan />} />       
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
