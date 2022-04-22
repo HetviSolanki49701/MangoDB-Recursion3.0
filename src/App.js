77cf12cc1b9b84bd6138402bd0aada6225d6f07e
Avish Jain <avishjain0@gmail.com>
2022-04-22 08:51:56 +0530

    diet

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import Home from "./components/Home/Home";
 import { BrowserRouter, Route, NavLink, Link, Routes,useNavigate } from "react-router-dom";
 import Alan from "./Alan";
 import Profile from "./components/Profile/Profile";
-import { BrowserRouter, Route, NavLink, Link, Routes } from "react-router-dom";
 import Exercise from "./components/Exercise/Exercise";
 import Room from "./components/Room/Room";
 import React, { useEffect } from 'react';
+import Exerciselist from "./components/ExerciseList/ExerciseList";
+import Diet from "./components/Diet/Diet";
 
 function App() {
   return (
@@ -16,6 +17,8 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/exercise" element={<Exercise />} />
+        <Route path="/list" element={<Exerciselist />} />
+        <Route path="/diet" element={<Diet />} />
         <Route path="/room" element={<Room />} />
       </Routes>
     </BrowserRouter>
